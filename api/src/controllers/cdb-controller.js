50d018c7d8c695752815ef24b86f86d4c0986a9f
Rodrigo do Nascimento Barbosa <rodrigob.dev@outlook.com>
2021-03-02 12:54:06 -0300

    Adicionar validações para os tipos dos parâmetros de entrada

--- api/src/controllers/cdb-controller.js
+++ api/src/controllers/cdb-controller.js
@@ -1,17 +1,18 @@
 const CdbModule = require('../modules/cdb-module');
+const moment = require('moment-timezone');
 class CdbController {
     async getCalculate(req, res) {
         try {
 
-            if (!req.query.investmentDate) {
+            if (!req.query.investmentDate || !moment(req.query.investmentDate, 'YYYY-MM-DD', true).isValid()) {
                 return res.status(400).send('invalid params[investmentDate]');
             }
 
-            if (!req.query.currentDate) {
+            if (!req.query.currentDate || !moment(req.query.currentDate, 'YYYY-MM-DD', true).isValid()) {
                 return res.status(400).send('invalid params[currentDate]');
             }
 
-            if (!req.query.cdbRate) {
+            if (!req.query.cdbRate || isNaN(req.query.cdbRate)) {
                 return res.status(400).send('invalid params[cdbRate]');
             }
 
